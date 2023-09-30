27b8baff4a68d69bdd8e6eedae0ba6d9fead6a67
Musab Sakhreyah <mosabkhaled1998@gmail.com>
2023-09-30 04:43:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(arr, evt) {
+    const newArr = []
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(`Thank you, ${arr[i]}, for the wonderful ${evt} gift!`);
+    }
+    return newArr;
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num--;
+    }
+}
